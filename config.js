1250c194f29fea6fbd98358088299cd98e46484c
Nadeen Poorna <nadeenpoorna405@gmail.com>
2025-09-23 14:56:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=dER3xRhL#TymvE0h759idx_cBXNROYFO19I-WEWf2SkAKCRN1H9s' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME,            
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME,    
+    DB_NAME: 'pramaaaataaaaaaaaajjaa',
 };
